d81171ef911f5a06ae76ddef068490b866dbd72e
oleh.krutii <oleh.krutii@justanswer.com>
2022-12-19 17:43:15 +0200

    add simple implementation of QuickFind UnionFind

---
+++ AlgorithmsPart1/Algorithms/Union/UnionFind/QuickFindUF.cs
@@ -0,0 +1,36 @@
+﻿namespace UnionFind
+{
+    /// <summary>
+    /// Quick find implementation of Union Find
+    /// </summary>
+    public class QuickFindUF
+    {
+        private readonly int[] _store;
+
+        public QuickFindUF(int size)
+        {
+            _store = Enumerable.Range(0, size).ToArray();
+        }
+
+        public int GetNumberOfConnectedSets()
+            => _store.Distinct().Count();
+
+        public bool Connected(int first, int second)
+            => _store[first] == _store[second];
+
+        public void Union(int first, int second)
+        {
+            if (Connected(first, second))
+                return;
+
+            var firstSetIndex = _store[first];
+            var secondSetIndex = _store[second];
+
+            for (var i = 0; i < _store.Length; ++i)
+            {
+                if (_store[i] == firstSetIndex)
+                    _store[i] = secondSetIndex;
+            }
+        }
+    }
+}
