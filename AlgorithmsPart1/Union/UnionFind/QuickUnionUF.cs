1503ea7e0982965657eeb9f4d957f6f14b8fe85d
oleh.krutii <oleh.krutii@justanswer.com>
2022-12-20 13:14:12 +0200

    make quickUnion better by using weighted tree approach

--- AlgorithmsPart1/Union/UnionFind/QuickUnionUF.cs
+++ AlgorithmsPart1/Union/UnionFind/QuickUnionUF.cs
@@ -2,16 +2,24 @@
 {
     /// <summary>
     /// Quick union implementation.
-    /// Find complexity: O(N) in worst case scenario
-    /// Union complexity: O(N) in worst case scenario
+    /// Find complexity: O(logN) in worst case scenario
+    /// Union complexity: O(logN), because size of each tree can only double.
     /// </summary>
     public class QuickUnionUF : IUnionFind
     {
         private readonly int[] _store;
+        private readonly int[] _weights;
 
         public QuickUnionUF(int size)
         {
-            _store = Enumerable.Range(0, size).ToArray();
+            _store = new int[size];
+            _weights = new int[size];
+
+            foreach (int i in Enumerable.Range(0, size))
+            {
+                _store[i] = i;
+                _weights[i] = 1;
+            }
         }
 
         public int GetNumberOfConnectedSets()
@@ -25,7 +33,22 @@
             var firstElementRoot = GetRoot(first);
             var secondElementRoot = GetRoot(second);
 
-            _store[firstElementRoot] = secondElementRoot;
+            if (firstElementRoot == secondElementRoot)
+                return;
+
+            var firstRootWeight = _weights[firstElementRoot];
+            var secondRootWeight = _weights[secondElementRoot];
+
+            if (firstRootWeight < secondRootWeight)
+            {
+                _store[firstElementRoot] = secondElementRoot;
+                _weights[secondElementRoot] += _weights[firstElementRoot];
+            }
+            else
+            {
+                _store[secondElementRoot] = firstElementRoot;
+                _weights[firstElementRoot] += _weights[secondElementRoot];
+            }
         }
 
         private int GetRoot(int child)
