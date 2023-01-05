5270fe6ffe2211b1b8458a8f9a67e45ab976656e
olehk <olegkrutiy@gmail.com>
2023-01-05 22:53:46 +0200

    implement linked list based queue

---
+++ AlgorithmsPart1/Collections/EzCollections.Tests/LinkedListBasedQueueTests.cs
@@ -0,0 +1,92 @@
+﻿using EzCollections.Queues;
+using FluentAssertions;
+
+namespace EzCollections.Tests
+{
+    public class LinkedListBasedQueueTests
+    {
+        [Fact]
+        public void Queue_ElementProvided_ElementQueued()
+        {
+            // Arrange
+            var queue = new LinkedListBasedQueue<int>();
+
+            // Act
+            queue.Enqueue(8);
+
+            // Assert
+            queue.IsEmpty.Should().BeFalse();
+            queue.First.Should().Be(8);
+            queue.Last.Should().Be(8);
+            queue.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void Queue_NotEmptyQueue_CorrectOrderIsPreserved()
+        {
+            // Arrange
+            var queue = new LinkedListBasedQueue<int>();
+            queue.Enqueue(8);
+            queue.Enqueue(88);
+
+            // Act
+            queue.Enqueue(888);
+
+            // Assert
+            queue.First.Should().Be(8);
+            queue.Last.Should().Be(888);
+            queue.Should().HaveCount(3);
+            queue.Should().BeEquivalentTo(new int[] { 8, 88, 888 });
+        }
+
+        [Fact]
+        public void Dequeue_QueueIsEmpty_ThrowsInvalidOperation()
+        {
+            // Arrange
+            var queue = new LinkedListBasedQueue<int>();
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => queue.Dequeue());
+        }
+
+        [Fact]
+        public void Dequeue_OneElementExist_QueueBecomesEmpty()
+        {
+            // Arrange
+            var queue = new LinkedListBasedQueue<int>();
+            queue.Enqueue(8);
+
+            // Act
+            var actual = queue.Dequeue();
+
+            // Assert
+            actual.Should().Be(8);
+            queue.IsEmpty.Should().BeTrue();
+            queue.Should().HaveCount(0);
+            ((Func<int>)(() => queue.First)).Should().Throw<InvalidOperationException>();
+            ((Func<int>)(() => queue.Last)).Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void Dequeue_SomeElementsExist_CloseToRealUseSimulation()
+        {
+            // Arrange
+            var queue = new LinkedListBasedQueue<int>();
+            var roleModel = new Queue<int>();
+            var random = new Random();
+
+            // Act
+            foreach (var i in Enumerable.Range(0, 10))
+            {
+                queue.Enqueue(i);
+                roleModel.Enqueue(i);
+
+                if (random.Next(1) == 1)
+                    queue.Dequeue().Should().Be(roleModel.Dequeue());
+            }
+
+            // Assert
+            queue.Should().BeEquivalentTo(roleModel.AsEnumerable());
+        }
+    }
+}
---
+++ AlgorithmsPart1/Collections/EzCollections/Queues/LinkedListBasedQueue.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+
+namespace EzCollections.Queues
+{
+    public class LinkedListBasedQueue<T> : IEnumerable<T>
+    {
+        private static readonly Node<T> NullNode = new Node<T>(default, null);
+
+        private Node<T> _first = NullNode;
+        private Node<T> _last = NullNode;
+        private int _count = 0;
+
+        public bool IsEmpty => _first == NullNode;
+        public int Count => _count;
+
+        public T First
+        {
+            get
+            {
+                ThrowIfQueueIsEmpty();
+
+                return _first.Element;
+            }
+        }
+
+        public T Last
+        {
+            get
+            {
+                ThrowIfQueueIsEmpty();
+
+                return _last.Element;
+            }
+        }
+
+        public void Enqueue(T element)
+        {
+            var node = new Node<T>(element, NullNode);
+
+            if (IsEmpty)
+            {
+                _first = _last = node;
+                return;
+            }
+
+            _last.Next = node;
+            _last = node;
+            _count++;
+        }
+
+        public T Dequeue()
+        {
+            ThrowIfQueueIsEmpty();
+
+            var value = _first.Element;
+
+            _first = _first.Next;
+
+            return value;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var iterator = _first;
+
+            while(iterator != NullNode)
+            {
+                yield return iterator.Element;
+                iterator = iterator.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        private void ThrowIfQueueIsEmpty()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Queue is empty.");
+        }
+
+        private record Node<T>
+        {
+            public Node(T element, Node<T> next)
+            {
+                Element = element;
+                Next = next;
+            }
+
+            public T Element { get; }
+            public Node<T> Next { get; set; }
+        }
+    }
+}
