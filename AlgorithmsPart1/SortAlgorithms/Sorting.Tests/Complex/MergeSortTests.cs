4ed7f4947af499ddda992eae911cb2d11d7f8b4e
olehk <olegkrutiy@gmail.com>
2023-02-06 00:51:30 +0200

    implement rough sketch of MergeSort

---
+++ AlgorithmsPart1/SortAlgorithms/Sorting.Tests/Complex/MergeSortTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Sorting.Complex;
+
+namespace Sorting.Tests.Complex
+{
+    public class MergeSortTests
+    {
+        [Fact]
+        public void Merge_TwoSortedArraysProvided_MergeThemCorrectly()
+        {
+            // Arrange
+            var firstSorted = new[] { 1, 2, 3 };
+            var secondSorted = new[] { 1, 5, 8 };
+            var result = firstSorted.Concat(secondSorted).ToArray();
+            var auxiliry = new int[result.Length];
+
+            // Act
+            MergeSort.Merge(result, auxiliry, 0, 2, 5, Comparer<int>.Default.Compare);
+
+            // Assert
+            result.Should().BeInAscendingOrder();
+        }
+
+        [Fact]
+        public void Sort_RandomUnsortedArrayProvided_SortsPerfectly()
+        {
+            // Arrange
+            var unsorted = Enumerable.Range(1, 10).Select(i => new Random().Next(0, 11)).ToList();
+
+            // Act
+            MergeSort.Sort(unsorted);
+
+            // Assert
+            unsorted.Should().BeInAscendingOrder();
+        }
+    }
+}
---
+++ AlgorithmsPart1/SortAlgorithms/Sorting/Complex/MergeSort.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Contracts;
+
+namespace Sorting.Complex
+{
+    public class MergeSort
+    {
+        public static void Sort<T>(IList<T> list) where T : IComparable
+            => Sort(list, (a, b) => a.CompareTo(b));
+
+        public static void Sort<T>(IList<T> list, IComparer<T> comparer)
+            => Sort(list, comparer.Compare);
+
+        public static void Sort<T>(IList<T> list, Func<T, T, int> comparer)
+        {
+            var auxiliry = new T[list.Count];
+
+            Sort(list, auxiliry, 0, list.Count - 1, comparer);
+        }
+
+        private static void Sort<T>(IList<T> original, IList<T> auxiliry, int low, int high, Func<T, T, int> comparer)
+        {
+            if (high <= low)
+            {
+                return;
+            }
+
+            int mid = (low + high) / 2;
+
+            Sort(original, auxiliry, low, mid, comparer);
+            Sort(original, auxiliry, mid + 1, high, comparer);
+
+            Merge(original, auxiliry, low, mid, high, comparer);
+        }
+
+
+
+        public static void Merge<T>(IList<T> original, IList<T> auxilary, int low, int mid, int high, Func<T, T, int> comparer)
+        {
+            for (int k = low; k <= high; k++)
+            {
+                auxilary[k] = original[k];
+            }
+
+            int i = low;
+            int j = mid + 1;
+
+            for (int k = low; k <= high; k++)
+            {
+                if (i > mid)
+                {
+                    original[k] = auxilary[j++];
+                }
+                else if (j > high)
+                {
+                    original[k] = auxilary[i++];
+                }
+                else if (auxilary[i].IsLessThan(auxilary[j], comparer))
+                {
+                    original[k] = auxilary[i++];
+                }
+                else
+                {
+                    original[k] = auxilary[j++];
+                }
+            }
+        }
+    }
+}
