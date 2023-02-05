a6b52a8c519079f0a55e40556f48f7500efe01a8
olehk <olegkrutiy@gmail.com>
2023-02-05 23:13:38 +0200

    add one test

--- AlgorithmsPart1/SortAlgorithms/ConvexHull.Tests/CoordinateSystemTests.cs
+++ AlgorithmsPart1/SortAlgorithms/ConvexHull.Tests/CoordinateSystemTests.cs
@@ -1,9 +1,11 @@
+using FluentAssertions;
+
 namespace ConvexHull.Tests
 {
     public class CoordinateSystemTests
     {
         [Fact]
-        public void CalculateConvexHull_AllFivePointsCreateConvexHull_ReturnsAllFivePoints()
+        public void CalculateConvexHull_SixPointsProvided_ReturnsThreePointsConvexHull()
         {
             // Arrange
             var system = new CoordinateSystem()
@@ -16,13 +18,16 @@ namespace ConvexHull.Tests
                 new Point(4, 2)
             };
 
-            var comparer = new PolarAngleComparer(new Point(-1, 0));
-            var result = comparer.Compare(new Point(5, 4), new Point(3, 8));
-
             // Act
             var convexHull = system.CalculateConvexHull();
 
             // Assert
+            convexHull.Should().BeEquivalentTo(new[]
+            {
+                new Point(3, 8),
+                new Point(-1, 0),
+                new Point(8, 1)
+            });
         }
     }
 }
